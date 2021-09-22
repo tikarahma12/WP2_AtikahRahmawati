b9596ed3d3aa571dc5fca1b8207dba243da4f150
Atikah Rahmawati <90938907+tikarahma12@users.noreply.github.com>
2021-09-22 13:15:08 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,14 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+    echo"<h1>Hallo World</h1>";
+    echo"<hr></hr>";
+    echo"<b>Nama saya Atikah Rahmawati</b><br>";
+    echo"Saya adalah Mahasiswi Sistem Informasi Universitas Bina Sarana Informatika <br>";
+    echo"Saya berasal dari Kebumen <br>";
+    echo"Saya tinggal di Rawa Buaya <br>";
+    echo"Hobi saya memasak <br>";
+    }
+}
